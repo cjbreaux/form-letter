dfb005e0bfc4613564d4f01fa78b074a64a70360
Chris Breaux and Tien Le <tien@>
2019-01-16 12:53:23 -0800

    capitalize first letter in forms, recolor submit button and correct some spacing issues, hide the letter in a well

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var inputName1 = $("input#inputTo").val();
-    var inputName2 = $("input#inputFrom").val();
+    var inputName1 = $("input#inputTo").val().charAt(0).toUpperCase() + $("input#inputTo").val().substr(1);
+    var inputName2 = $("input#inputFrom").val().charAt(0).toUpperCase() + $("input#inputFrom").val().substr(1);
 
-    $(".input1").append(inputName1);
-    $(".input2").append(inputName2);
+    $(".input1").text(inputName1);
+    $(".input2").text(inputName2);
+
+    $(".well").show();
 
     event.preventDefault();
   });
